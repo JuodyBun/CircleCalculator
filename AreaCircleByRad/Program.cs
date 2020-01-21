b076eb1daf0be4a3d098e3201b67eb02c7920198
Juody Bun <bjuody@gmail.com>
2020-01-21 08:06:13 -0600

    Add project files.

---
+++ AreaCircleByRad/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AreaCircleByRad
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine( UserInput());
+            Console.WriteLine( Area());
+        }
+
+        public static string UserInput()
+        {
+
+            Console.WriteLine("Hello! I can help you calculate the area, diameter and circumference of a circle by its' radius. What is the radius?");
+            string radius = Console.ReadLine();
+            double userRad = Convert.ToDouble(radius);
+            return userRad;
+            
+        }
+
+        public double Area(double userRad)
+        {
+            double area = 3.14 * Math.Pow(userRad, 2);
+            Console.WriteLine($"The area is + {area}.");
+            return area;
+        }
+
+        public double Diameter(double userRad)
+        {
+            double diameter = userRad * 2;
+            Console.WriteLine($"The diameter is {diameter}.");
+            return diameter;
+        }
+
+        public double Circumference(double diameter)
+        {
+            double circumference = 3.14 * diameter;
+            Console.WriteLine($"The circumference is {circumference}");
+            return circumference;
+        }
+    }
+}
